60e00f3e94a4d86e57c7a5fa3fb1eafbf5ad3fcb
Sk264 <sdkumbhalkar07@gmail.com>
2023-06-03 14:05:44 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
+AUTHOR_USER_NAME = "Sk264"
+SRC_REPO = "https://github.com/Sk264/BookRecommend.git"
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Movie Recommender System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/Sk264/BookRecommend.git",
+    author_email="sdkumbhalkar07@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
